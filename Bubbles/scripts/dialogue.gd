aef8b2658345b7bed40f886b71f785717552920e
DevvEn <69240278+oDevvEn@users.noreply.github.com>
2025-01-21 16:19:03 +0000

    Dialogue System :de:

--- Bubbles/scripts/dialogue.gd
+++ Bubbles/scripts/dialogue.gd
@@ -1,11 +1,24 @@
 extends Control
 
-@onready var picture : TextureRect = $Background/Picture
-@onready var speaker : RichTextLabel = $Background/Name
-@onready var dialogue : RichTextLabel = $Background/Dialogue
+signal click(value)
 
-func setup(p, n:String, d:String) -> void:
-	picture.texture = p
-	speaker.text = n
-	dialogue.text = d
+func setupDialogue(speakerPicture, speakerName:String, dialog:String) -> void:
+	var picture : TextureRect = $Background/Picture
+	var speaker : RichTextLabel = $Background/Name
+	var dialogue : RichTextLabel = $Background/Dialogue
 
+	picture.texture = speakerPicture
+	speaker.text = "[b]%s[/b]" % speakerName
+	dialogue.text = dialog
+
+func setupButtons(choices:Array) -> void:
+	var count = 0
+	for choice in choices:
+		var button : Button = Button.new()
+		button.text = choice
+		button.size = Vector2(240, 60)
+		button.position = Vector2(1920 - 300 - count*300, 240)
+		button.add_theme_font_size_override("font_size", 36)
+		button.connect("pressed", func(): click.emit(count))
+		add_child(button)
+		count += 1
