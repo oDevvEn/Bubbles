8eb14cc7f1eee8ecca21e076bc12219c37a8dc1e
Harding12 <s24000007727@leggott.ac.uk>
2025-01-23 09:19:27 +0000

    Add files via upload

--- Bubbles/scripts/VillagerDialogue.gd
+++ Bubbles/scripts/VillagerDialogue.gd
@@ -3,7 +3,7 @@ extends CollisionShape2D
 @onready var dialogue : PackedScene = preload("res://scenes/dialogue.tscn") #debug
 const FISH = preload("res://assets/DialogueBoxFish4.png")
 var inCollider = false
-var d : CanvasLayer = dialogue.instantiate()
+@onready var d : CanvasLayer = dialogue.instantiate()
 
 func _on_interactive_ui_body_entered(body: Node2D) -> void:
 	inCollider = true
@@ -22,15 +22,13 @@ func _process(delta: float) -> void:
 func First1(choice):
 	match choice:
 		0:
-			d.setupDialogue(FISH, "You seem confused, how's your memory holding up?")
+			d.setupDialogue(FISH,"Villager", "You seem confused, how's your memory holding up?")
 			d.setupButtons(["I can't think of anything", "Where am I?"])
 			d.connect("click", Intermediary)
-			add_child(d)
 		1:
-			d.setupDialogue(FISH, "Why, you're on Dock Island, of course!")
+			d.setupDialogue(FISH,"Villager", "Why, you're on Dock Island, of course!")
 			d.setupButtons(["What's that?"])
 			d.connect("click", Second2)
-			add_child(d)
 
 func Intermediary(choice):
 	match choice:
@@ -40,54 +38,49 @@ func Intermediary(choice):
 			First1(1)
 
 func Second1():
-		d.setupDialogue(FISH, "Oh, you're fully gone. Wow! I haven't met a newbie before!")
+		d.setupDialogue(FISH,"Villager", "Oh, you're fully gone. Wow! I haven't met a newbie before!")
 		d.setupButtons(["What's that supposed to mean?", "What's going on?"])
 		d.connect("click", Third1)
-		add_child(d)
 		
 		
 		
 func Second2(choice):
-		d.setupDialogue(FISH, "The main fishing island of this place. You've got the dock off to the west and the shop down to the east")
+		d.setupDialogue(FISH,"Villager", "The main fishing island of this place. You've got the dock off to the west and the shop down to the east")
 		d.setupButtons(["You're a shark, why would you be fishing?"])
 		d.connect("click", Third2)
-		add_child(d)
 		
 func Third1(choice):
 	match choice:
 		0:
-			d.setupDialogue(FISH, "Oh, I meant no harm by it. Please don't take offense, I've just never met someone new here before!")
+			d.setupDialogue(FISH,"Villager", "Oh, I meant no harm by it. Please don't take offense, I've just never met someone new here before!")
 			d.setupButtons(["What's going on?"])
 			d.connect("click", Fourth1)
-			add_child(d)
 		1:
 			Fourth1(0)
 
 func Fourth1(choice):
-		d.setupDialogue(FISH, "Right. We've all shown up here without any memory of what's going on. Supposedly, the only way to regain any memory is to get 'The Orb' and look through into it")
+		d.setupDialogue(FISH,"Villager", "Right. We've all shown up here without any memory of what's going on. Supposedly, the only way to regain any memory is to get 'The Orb' and look through into it")
 		d.setupButtons(["And where is this orb?"])
 		d.connect("click", Fifth1)
-		add_child(d)
 
 func Fifth1(choice):
-		d.setupDialogue(FISH, "For just a few thousand coins in the shop!")
+		d.setupDialogue(FISH,"Villager", "For just a few thousand coins in the shop!")
 		d.setupButtons(["Behind a pay wall, makes sense..."])
 		d.connect("click", Sixth1)
-		add_child(d)
 		
 func Sixth1(choice):
-		d.setupDialogue(FISH, "Oh, don't worry. Look, here's a few coins. Go grab yourself a fishing rod and you can gather up some fish, and sell them at the shop to earn your thousands!")
+		d.setupDialogue(FISH,"Villager", "Oh, don't worry. Look, here's a few coins. Go grab yourself a fishing rod and you can gather up some fish, and sell them at the shop to earn your thousands!")
 		d.setupButtons(["Alright then"])
 		d.connect("click", End)
-		add_child(d)
+
 		
 		
 func End():
 	d.queue_free()
 		
 func Third2(choice):
-			d.setupDialogue(FISH, "You seem confused, how's your memory holding up?")
+			d.setupDialogue(FISH,"Villager", "You seem confused, how's your memory holding up?")
 			d.setupButtons(["I can't think of anything"])
 			d.connect("click", Second1)
-			add_child(d)
+
 		
